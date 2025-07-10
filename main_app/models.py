622f407057f90e366e8346037cba20ed432f6c61
Michelle Bollino <jackandzoe.rosen@gmail.com>
2025-07-09 21:29:38 -0400

    Created and registered Viewings model.  Added display.  Fixing CSS

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.urls import reverse
 
+PERSONAL_RATING = (
+    (1, '1 Star'),
+    (2, '2 Stars'),
+    (3, '3 Stars'),
+    (4, '4 Stars'),
+    (5, '5 Stars'),
+)
+
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=100)
@@ -15,3 +23,16 @@ class Movie(models.Model):
     
     def get_absolute_url(self):
         return reverse('movie_detail', kwargs={'movie_id': self.id})
+    
+class Viewing(models.Model):
+    view_date = models.DateField()
+    personal_rating = models.IntegerField(
+        max_length=1,
+        choices=PERSONAL_RATING, 
+        default=PERSONAL_RATING[0][0]
+    )
+
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.view_date} - {self.get_personal_rating_display()}"
